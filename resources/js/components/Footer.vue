7e79cb9095944a5b0f755f3fafe16ae869917bb7
Nicolas2595 <nmorelli10@gmail.com>
2021-07-30 16:26:01 +0200

    API + Axios

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="text-center">
+      &copy; Boolean Careers
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+</style>
