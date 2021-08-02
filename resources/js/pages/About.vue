52246e47d14ac0342f03257fc57640e00d839c58
Nicolas2595 <nmorelli10@gmail.com>
2021-08-02 17:07:08 +0200

    vue-router + rotte

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="my-5">
+      <h1 class="text-center">Chi siamo</h1>
+      <p class="text-center">Testo chi siamo</p>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style>
+</style>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="my-5">
+      <h1 class="text-center">Home Page</h1>
+      <p class="text-center">Testo della Home page</p>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style>
+</style>
